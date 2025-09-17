b51f77ccac462f72bf09c1793322dd024197fd65
karonpro <kaggaronald1@gmail.com>
2025-09-17 00:22:30 -0700

    Connect Django to Railway PostgreSQL

--- teba/settings.py
+++ teba/settings.py
@@ -50,12 +50,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'teba.wsgi.application'
 
-import dj_database_url
+
 import os
+import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=600)
+    'default': dj_database_url.config(
+        default="postgresql://postgres:cRiXjJXaMAVFNFZnZowlFOMIdRmQwOGL@switchback.proxy.rlwy.net:50591/railway",
+        conn_max_age=600
+    )
 }
+
 
 
 AUTH_PASSWORD_VALIDATORS = []
