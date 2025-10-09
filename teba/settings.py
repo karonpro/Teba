773037986ccd10c6843bcfbf74cc8a4a70955b37
karonpro <kaggaronald1@gmail.com>
2025-10-09 05:39:18 -0700

    Merged conflicting migrations in transactions app

--- teba/settings.py
+++ teba/settings.py
@@ -21,7 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'transactions',
+    'transactions','django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
